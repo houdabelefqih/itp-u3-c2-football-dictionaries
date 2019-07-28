3e9b62fe7f91e02eb4c84364e16f5345e183d965
Houda Belefqih <houdabelefqih@gmail.com>
2019-07-28 11:51:46 -0500

    assignment_2 complete, test 2 passed

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,15 @@
+from collections import OrderedDict
+
+
 def players_by_position(squads_list):
-    pass
+
+    player_dictionary = {}
+
+    player_keys_list = ['number', 'position', 'name', 'date_of_birth', 'caps', 'club', 'country', 'club_country',
+                        'year']
+
+    for player in squads_list:
+        single_player_dictionary = OrderedDict(sorted(zip(player_keys_list, player), key=lambda tup: tup[0]))
+        player_dictionary.setdefault(single_player_dictionary['position'], []).append(single_player_dictionary)
+
+    return player_dictionary
